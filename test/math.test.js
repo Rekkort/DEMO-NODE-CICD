f5c5f0b8c3d9375273d3895b4850949dc1e08438
Lenny Marcel <lennyfmarcel@gmail.com>
2021-08-30 10:39:04 +0200

    let test error

--- test/math.test.js
+++ test/math.test.js
@@ -5,7 +5,7 @@ describe('mathFileToTest.js tests', () => {
     describe('math.add() Test', () => {
         it('should equal 2', () => {
             const result = math.add(1, 1);
-            expect(result).to.equal(2);
+            expect(result).to.equal(3);
         });
         it('should equal 4', () => {
             const result = math.add(2, 2);
